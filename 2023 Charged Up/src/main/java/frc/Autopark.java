fed607ce74ce11b97b69ef94b40d11d106f0e0a6
kingtim17287 <timwplaysroblox@gmail.com>
2023-01-22 20:42:06 -0500

    added Autopark class and enum

---
+++ 2023 Charged Up/src/main/java/frc/Autopark.java
@@ -0,0 +1,19 @@
+package frc;
+
+public class Autopark {
+
+    public enum AutoBalanceStates {
+        DRIVE_FORWARD, 
+        PITCH_UP,
+        PITCH_DOWN,
+        STOP;
+    }
+    
+    AutoBalanceStates currentState = AutoBalanceStates.DRIVE_FORWARD;
+    
+    public boolean balance(){
+        
+        
+        return  true;
+    }
+}
