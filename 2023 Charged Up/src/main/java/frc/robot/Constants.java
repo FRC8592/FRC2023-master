45d8e3c5d5c4373850731dd45767f0474093d3fb
slyguy5646 <lpmonaghan06@yahoo.com>
2023-03-25 20:45:09 -0400

    working four bar with new linkage instead of chain

--- 2023 Charged Up/src/main/java/frc/robot/Constants.java
+++ 2023 Charged Up/src/main/java/frc/robot/Constants.java
@@ -96,7 +96,7 @@ public final class Constants {
     public static final int ELEVATOR_TILT_MOTOR_ID   = 15;
     public static final double LIFT_GEARBOX_RATIO    = 1.0 / 16.0;
     public static final double TILT_GEARBOX_RATIO    = 1.0 / 80.0 * 16.0 / 32.0;
-    public static final double TILT_MAX_ROTATIONS = -25.5;
+    public static final double TILT_MAX_ROTATIONS = -23.5; // -25.5
     public static final double LIFT_MAX_ROTATIONS = -80;
     public static final double LIFT_STOWED_ROTATIONS = 0;
     public static final double LIFT_MAX_INCHES = -63;
