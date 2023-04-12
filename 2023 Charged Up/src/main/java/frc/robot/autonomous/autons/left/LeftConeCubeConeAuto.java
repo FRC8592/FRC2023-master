48f4121e6a7f236da5b2b42a7e87e659efa27b90
IAbbasi04 <Ibrahimzabbasi04@gmail.com>
2023-04-12 19:39:32 -0400

    WIP

--- 2023 Charged Up/src/main/java/frc/robot/autonomous/autons/left/LeftConeCubeConeAuto.java
+++ 2023 Charged Up/src/main/java/frc/robot/autonomous/autons/left/LeftConeCubeConeAuto.java
@@ -16,7 +16,7 @@ import frc.robot.commands.PipelineCommand.Pipeline;
 import static frc.robot.autonomous.AutonomousPositions.*;
 
 public class LeftConeCubeConeAuto extends BaseAuto {
-    private TrajectoryConfig fastConfig = new TrajectoryConfig(4.0, 2.0);
+    private TrajectoryConfig fastConfig = new TrajectoryConfig(1.0, 2.0);
     private TrajectoryConfig slowConfig = new TrajectoryConfig(2.0, 1.0);
 
     private SwerveTrajectory C_TO_NEUTRAL_ZONE = generate(
@@ -117,37 +117,37 @@ public class LeftConeCubeConeAuto extends BaseAuto {
                 new FollowerCommand(drive, NEUTRAL_ZONE_TO_GP1),
                 new IntakeCommand(intake, true)
             ),
-            new JointCommand( // Move back to community
-                new FollowerCommand(drive, GP1_TO_COMMUNITY),
-                new LiftCommand(elevator, Heights.PRIME, 1.0, true),
-                new PipelineCommand(vision, Pipeline.APRIL_TAG)
-            ),
-            new JointCommand( // Score second piece
-                new FollowerCommand(drive, COMMUNITY_TO_B),
-                new LiftCommand(elevator, Heights.MID),
-                new ScoreCommand(intake, 0.25)
-            ),
-            new JointCommand( // Move to third piece
-                new FollowerCommand(drive, B_TO_NEUTRAL_ZONE),
-                new LiftCommand(elevator, Heights.STOWED, true),
-                new IntakeCommand(intake, 1.5, true),
-                new PipelineCommand(vision, Pipeline.CONE)
-            ),
-            new JointCommand( // Intake third piece
-                new FollowerCommand(drive, vision, NEUTRAL_ZONE_TO_GP2),
-                new IntakeCommand(intake, true)
-            ),
-            new JointCommand( // Move back to community
-                new FollowerCommand(drive, GP2_TO_COMMUNITY),
-                new LiftCommand(elevator, Heights.PRIME, 1.0, true),
-                new PipelineCommand(vision, Pipeline.RETRO_TAPE)
-            ),
-            new JointCommand( // Score third piece
-                new FollowerCommand(drive, vision, COMMUNITY_TO_A),
-                new LiftCommand(elevator, Heights.MID, true),
-                new ScoreCommand(intake, 0.5)
-            ),
-            new LiftCommand(elevator, Heights.STOWED) // Stow elevator
+            // new JointCommand( // Move back to community
+            //     new FollowerCommand(drive, GP1_TO_COMMUNITY),
+            //     new LiftCommand(elevator, Heights.PRIME, 1.0, true),
+            //     new PipelineCommand(vision, Pipeline.APRIL_TAG)
+            // ),
+            // new JointCommand( // Score second piece
+            //     new FollowerCommand(drive, COMMUNITY_TO_B),
+            //     new LiftCommand(elevator, Heights.MID),
+            //     new ScoreCommand(intake, 0.25)
+            // ),
+            // new JointCommand( // Move to third piece
+            //     new FollowerCommand(drive, B_TO_NEUTRAL_ZONE),
+            //     new LiftCommand(elevator, Heights.STOWED, true),
+            //     new IntakeCommand(intake, 1.5, true),
+            //     new PipelineCommand(vision, Pipeline.CONE)
+            // ),
+            // new JointCommand( // Intake third piece
+            //     new FollowerCommand(drive, vision, NEUTRAL_ZONE_TO_GP2),
+            //     new IntakeCommand(intake, true)
+            // ),
+            // new JointCommand( // Move back to community
+            //     new FollowerCommand(drive, GP2_TO_COMMUNITY),
+            //     new LiftCommand(elevator, Heights.PRIME, 1.0, true),
+            //     new PipelineCommand(vision, Pipeline.RETRO_TAPE)
+            // ),
+            // new JointCommand( // Score third piece
+            //     new FollowerCommand(drive, vision, COMMUNITY_TO_A),
+            //     new LiftCommand(elevator, Heights.MID, true),
+            //     new ScoreCommand(intake, 0.5)
+            // ),
+            // new LiftCommand(elevator, Heights.STOWED) // Stow elevator
         );
     }
 
